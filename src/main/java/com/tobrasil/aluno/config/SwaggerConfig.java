734053c4f4d027f2fefdce9520ee906a5ea0cb71
carlosdev@outlook.com.br <carlosdev@outlook.com.br>
2020-08-14 16:46:37 -0300

    Inserido SWAGGER

--- src/main/java/com/tobrasil/aluno/config/SwaggerConfig.java
+++ src/main/java/com/tobrasil/aluno/config/SwaggerConfig.java
@@ -22,7 +22,7 @@ public class SwaggerConfig {
 	@Bean
 	public Docket productApi() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("br.com.domvs")).paths(regex("/aluno.*")).build()
+				.apis(RequestHandlerSelectors.basePackage("com.tobrasil")).paths(regex("/aluno.*")).build()
 				.apiInfo(metaInfo());
 	}
 
